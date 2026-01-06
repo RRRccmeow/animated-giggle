b3b8146fa8510df17050dc80018bd3acaf0d40d1
RRRccmeow <fgit843@gmail.com>
2026-01-06 11:16:31 +0300

    Ignore .env and IDE files, use env var for SECRET_KEY

--- movies/settings.py
+++ movies/settings.py
@@ -12,17 +12,21 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m8!%jr3mp+6@l%--ssoy@h_2#0+v@^1rsmsfs2hy^xbe3h%l^o'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+if not SECRET_KEY:
+    raise RuntimeError("DJANGO_SECRET_KEY is not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
